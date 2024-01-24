0379aed58afb926c2fa062ca0d987d3b3a336161
Kevin Loughead <kvnloughead@gmail.com>
2024-01-24 16:04:00 -0500

    feat: add /reviews and /about-me routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
 import "./App.css";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
       </Routes>
     </div>
   );
