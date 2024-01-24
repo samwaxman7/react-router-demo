454862cef8928888d225dfe90fa524a6369c1bcb
Kevin Loughead <kvnloughead@gmail.com>
2024-01-24 16:01:05 -0500

    feat: a root route to display the dashboard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
