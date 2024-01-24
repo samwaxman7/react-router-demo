9b416698f8466a64b73df757318c43835bbbeb4c
Kevin Loughead <kvnloughead@gmail.com>
2024-01-24 16:57:44 -0500

    feat: add nested about-us routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,12 @@ import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
 import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
 import "./App.css";
 
 function App() {
@@ -14,8 +19,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="mission" element={<SiteMission />} />
+          <Route path="history" element={<SiteHistory />} />
+        </Route>
       </Routes>
     </div>
   );
