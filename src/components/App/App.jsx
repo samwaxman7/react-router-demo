5a874b0e34a120821aeed898bfae685b64537630
Kevin Loughead <kvnloughead@gmail.com>
2024-01-30 14:56:24 -0500

    feat: add dynamic review routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
+import Review from "../Review/Review";
 import AboutMe from "../AboutMe/AboutMe";
 import Contact from "../AboutMe/Contact";
 import Hobbies from "../AboutMe/Hobbies";
@@ -13,12 +15,32 @@ import SiteMission from "../AboutUs/SiteMission";
 import "./App.css";
 
 function App() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // Fetch the review data from the server.
+    fetch("https://api.nomoreparties.co/emoji-critic-ens")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // Pass the parsed response body to the setter function.
+        setReviews(data);
+      })
+      .catch(console.error);
+    // An empty dependency array means the hook only runs when
+    // component launches.
+  }, []);
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route
+          path="/reviews/:reviewId"
+          element={<Review reviews={reviews} />}
+        />
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
           <Route path="hobbies" element={<Hobbies />} />
